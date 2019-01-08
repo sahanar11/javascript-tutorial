0bd6856aba241eae3a14ca520513391b25138d64
B M <sahanar1191@gmail.com>
2019-01-08 22:06:07 +0530

    updated console docs

--- 02-operators/console.js
+++ 02-operators/console.js
@@ -4,9 +4,9 @@
  	  
 //Enables you to interact with a web page by executing JavaScript expressions in the context of the page
 
-Console.log("it logs the message on to console"); // - just an information ..  is a method used to log/write the messages on console
+console.log("it logs the message on to console"); // - just an information ..  is a method used to log/write the messages on console
 
-Console.warn("its a warning message"); // - prints warning you need to watch out for it
+console.warn("its a warning message"); // - prints warning you need to watch out for it
 
 console.error("Error!. Check Again");  // - indicates some serious issue. Need to be corrected..
 
